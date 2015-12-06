7c24d97e071964fce4619b4ecbfe439644e11ebc
admkopec <adm.kopec@gmail.com>
2015-12-06 19:55:10 +0100

    Updated build script

--- build.sh
+++ build.sh
@@ -3,10 +3,17 @@ export PREFIX="/opt/cross";
 export TARGET=i686-elf;
 export PATH="/opt/cross/bin:$PATH";
 
-i686-elf-as boot.s -o build/boot.o;
-i686-elf-g++ -c kernel.cpp -o build/kernel.o -ffreestanding -O2 -Wall -Wextra -fno-exceptions -fno-rtti;
-i686-elf-gcc -T build/linker.ld -o build/product/BetaOS.bin -ffreestanding -O2 -nostdlib build/boot.o build/kernel.o -lgcc;
+if ! i686-elf-as boot.s -o build/boot.o; then
+exit 1;
+fi
+if ! i686-elf-g++ -c kernel.cpp -o build/kernel.o -ffreestanding -O2 -Wall -Wextra -fno-exceptions -fno-rtti; then
+exit 2;
+fi
+if ! i686-elf-gcc -T build/linker.ld -o build/product/BetaOS.bin -ffreestanding -O2 -nostdlib build/boot.o build/kernel.o -lgcc; then
+exit 3;
+fi
 cp build/product/BetaOS.bin build/product/ISO/boot;
 cd build/product/ISO;
 rm BetaOS.iso;
 grub-mkrescue -o BetaOS.iso ./;
+exit 0;
