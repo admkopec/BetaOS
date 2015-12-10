6a8d4c1d8c8cbdbc27adb948886f758d23e4841c
admkopec <admkopec@gmail.com>
2015-12-10 11:39:40 +0100

    Some tests

--- build.sh
+++ build.sh
@@ -2,15 +2,14 @@
 export PREFIX="/opt/cross";
 export TARGET=i686-elf;
 export PATH="/opt/cross/bin:$PATH";
-export SYSROOT="/Users/adamkopec/Documents/OS/sysroot/";
-
+##-g -O2 -w -trigraphs -fno-builtin  -fno-exceptions -fno-stack-protector -O0 -m32  -fno-rtti -nostdlib -nodefaultlibs
 if ! i686-elf-as boot.s -o build/boot.o -Iinclude; then
 exit 1;
 fi
-if ! i686-elf-g++ -c kernel.cpp -o build/kernel.o -ffreestanding -fbuiltin -Wall -Wextra -fno-exceptions -fno-rtti -Iinclude; then
+if ! i686-elf-g++ -c kernel.cpp -o build/kernel.o -ffreestanding -fbuiltin -Wall -O2 -Wextra -fno-exceptions -m32 -fno-rtti -nostdlib -nodefaultlibs -I sysroot/include; then
 exit 2;
 fi
-if ! i686-elf-gcc -T build/linker.ld -o build/product/BetaOS.bin -ffreestanding -fbuiltin -Wall -Wextra -nostdlib build/boot.o build/kernel.o -lgcc; then
+if ! i686-elf-gcc -T build/linker.ld -o build/product/BetaOS.bin -ffreestanding -fbuiltin -Wall -O2 -Wextra -nostdlib -m32 -nodefaultlibs build/boot.o build/kernel.o -lgcc; then
 exit 3;
 fi
 cp build/product/BetaOS.bin build/product/ISO/boot;
