9b2091f4936c075d478d1b4dadbe81df4170bb21
admkopec <adm.kopec@gmail.com>
2015-12-16 17:12:04 +0100

    Added a few PIC functions

---
+++ kernel/arch/i386/pic.c
@@ -0,0 +1,97 @@
+//
+//  pic.c
+//  OS
+//
+//  Created by Adam Kopeć on 12/16/15.
+//  Copyright © 2015 Adam Kopeć. All rights reserved.
+//
+
+#include <stdio.h>
+#include <kernel/arch/pic.h>
+#include <kernel/interrupts.h>
+
+static uint16_t __pic_get_irq_reg(int ocw3)
+{
+    /* OCW3 to PIC CMD to get the register values.  PIC2 is chained, and
+     * represents IRQs 8-15.  PIC1 is IRQs 0-7, with 2 being the chain */
+    outb(PIC1_CMD, ocw3);
+    outb(PIC2_CMD, ocw3);
+    return (inb(PIC2_CMD) << 8) | inb(PIC1_CMD);
+}
+
+void PIC_sendEOI(unsigned char irq)
+{
+    if(irq >= 8)
+        outb(PIC2_COMMAND,PIC_EOI);
+    
+    outb(PIC1_COMMAND,PIC_EOI);
+}
+
+void PIC_remap(int offset1, int offset2)
+{
+    unsigned char a1, a2;
+    
+    a1 = inb(PIC1_DATA);                        // save masks
+    a2 = inb(PIC2_DATA);
+    
+    outb(PIC1_COMMAND, ICW1_INIT+ICW1_ICW4);  // starts the initialization sequence (in cascade mode)
+    io_wait();
+    outb(PIC2_COMMAND, ICW1_INIT+ICW1_ICW4);
+    io_wait();
+    outb(PIC1_DATA, offset1);                 // ICW2: Master PIC vector offset
+    io_wait();
+    outb(PIC2_DATA, offset2);                 // ICW2: Slave PIC vector offset
+    io_wait();
+    outb(PIC1_DATA, 4);                       // ICW3: tell Master PIC that there is a slave PIC at IRQ2 (0000 0100)
+    io_wait();
+    outb(PIC2_DATA, 2);                       // ICW3: tell Slave PIC its cascade identity (0000 0010)
+    io_wait();
+    
+    outb(PIC1_DATA, ICW4_8086);
+    io_wait();
+    outb(PIC2_DATA, ICW4_8086);
+    io_wait();
+    
+    outb(PIC1_DATA, a1);   // restore saved masks.
+    outb(PIC2_DATA, a2);
+}
+
+void IRQ_set_mask(unsigned char IRQline) {
+    uint16_t port;
+    uint8_t value;
+    
+    if(IRQline < 8) {
+        port = PIC1_DATA;
+    } else {
+        port = PIC2_DATA;
+        IRQline -= 8;
+    }
+    value = inb(port) | (1 << IRQline);
+    outb(port, value);
+}
+
+void IRQ_clear_mask(unsigned char IRQline) {
+    uint16_t port;
+    uint8_t value;
+    
+    if(IRQline < 8) {
+        port = PIC1_DATA;
+    } else {
+        port = PIC2_DATA;
+        IRQline -= 8;
+    }
+    value = inb(port) & ~(1 << IRQline);
+    outb(port, value);
+}
+
+/* Returns the combined value of the cascaded PICs irq request register */
+uint16_t pic_get_irr(void)
+{
+    return __pic_get_irq_reg(PIC_READ_IRR);
+}
+
+/* Returns the combined value of the cascaded PICs in-service register */
+uint16_t pic_get_isr(void)
+{
+    return __pic_get_irq_reg(PIC_READ_ISR);
+}
--- kernel/include/version.h
+++ kernel/include/version.h
@@ -13,6 +13,6 @@
 #define VERSION_MAJOR "0"
 #define VESRION_MINOR "9.2"
 #define VERSION_COPYRIGHT "Copyright (c) 2015 Adam Kopec. All rights reserved."
-#define VERSION_BUILD "15.12.15.2"
+#define VERSION_BUILD "15.12.16.1"
 
 #endif /* version_h */
